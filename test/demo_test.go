8f516535cdb8837298d0c5c9d1621ea885486119
weiyu <weiyu005@ke.com>
2019-12-13 18:31:31 +0800

    init

--- test/demo_test.go
+++ test/demo_test.go
@@ -16,3 +16,15 @@ func tes1() {
 	defer fmt.Println("======defer in test1")
 	fmt.Println("======end test1")
 }
+
+func TestPointer(t *testing.T) {
+	i, j := 12, 22
+	p := &i         //指向i
+	fmt.Println(*p) //通过指针获取i的值
+	*p = 21         //通过指针设置i的值
+	fmt.Println(i)
+
+	p = &j       //指向j
+	*p = *p / 11 //通过指针对j进行除法运算
+	fmt.Println(j)
+}
