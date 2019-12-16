7edad936dbfee317d649c894626d6db3e9782c96
weiyu <weiyu005@ke.com>
2019-12-16 11:10:21 +0800

    添加数组及切片示例

--- test/array_test.go
+++ test/array_test.go
@@ -20,6 +20,18 @@ func TestSlice(t *testing.T) {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	fmt.Println(primes[1:4])
 
+	//截取切片使其长度为0
+	s := primes[:0]
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+
+	//扩展其长度
+	s = s[:4]
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+
+	//舍弃前两个值
+	s = s[2:]
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+
 	//更改切片的元素会修改其底层数组中的对应元素
 	name := [4]string{"test1", "test2", "test3", "test4"}
 	a := name[0:2]
