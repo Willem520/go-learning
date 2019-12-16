2b01aa38510c4a2117bc0a5d9a20d019b0f84106
weiyu <weiyu005@ke.com>
2019-12-16 10:52:38 +0800

    添加数组及切片示例

--- test/array_test.go
+++ test/array_test.go
@@ -19,4 +19,14 @@ func TestArray(t *testing.T) {
 func TestSlice(t *testing.T) {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	fmt.Println(primes[1:4])
+
+	//更改切片的元素会修改其底层数组中的对应元素
+	name := [4]string{"test1", "test2", "test3", "test4"}
+	a := name[0:2]
+	b := name[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "test0"
+	fmt.Println(a, b)
+	fmt.Println(name)
 }
