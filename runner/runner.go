145bb4914c0d794fc5d8b79b5665d099ff49bced
willem <weiyu1990520@163.com>
2020-07-09 00:17:34 +0800

    添加runner示例

---
+++ runner/runner.go
@@ -0,0 +1,69 @@
+package runner
+
+import (
+	"errors"
+	"os"
+	"os/signal"
+	"time"
+)
+
+type Runner struct {
+	interrupt chan os.Signal
+	complete  chan error
+	timeout   <-chan time.Time
+	task      []func(int)
+}
+
+var ErrTimeout = errors.New("received timeout")
+
+var ErrInterrupt = errors.New("received interrupt")
+
+func New(d time.Duration) *Runner {
+	return &Runner{
+		interrupt: make(chan os.Signal, 1),
+		complete:  make(chan error),
+		timeout:   time.After(d),
+	}
+}
+
+func (r *Runner) Add(tasks ...func(int)) {
+	r.task = append(r.task, tasks...)
+}
+
+//执行所有任务，并监视通道事件
+func (r *Runner) Start() error {
+	signal.Notify(r.interrupt, os.Interrupt)
+
+	go func() {
+		r.complete <- r.run()
+	}()
+
+	select {
+	case err := <-r.complete:
+		return err
+	case <-r.timeout:
+		return ErrTimeout
+	}
+}
+
+// 执行已注册对任务
+func (r *Runner) run() error {
+	for id, task := range r.task {
+		if r.gotInterrupt() {
+			return ErrInterrupt
+		}
+		task(id)
+	}
+	return nil
+}
+
+//验证是否接收到了中断信号
+func (r *Runner) gotInterrupt() bool {
+	select {
+	case <-r.interrupt:
+		signal.Stop(r.interrupt)
+		return true
+	default:
+		return false
+	}
+}
