24e2550643f54793e7c8500127fdd3394cc543a1
willem <weiyu1990520@163.com>
2020-07-12 20:53:32 +0800

    添加pool示例

---
+++ pool/Pool.go
@@ -0,0 +1,93 @@
+package pool
+
+import (
+	"errors"
+	"io"
+	"log"
+	"sync"
+)
+
+/*
+author：willem
+description：
+date：2020-07-12 20:29
+*/
+
+type Pool struct {
+	m         sync.Mutex
+	resources chan io.Closer
+	factory   func() (io.Closer, error)
+	closed    bool
+}
+
+var ErrPoolClosed = errors.New("pool has been closed")
+
+/**
+创建指定大小的池
+*/
+func New(fn func() (io.Closer, error), size uint) (*Pool, error) {
+	if size <= 0 {
+		return nil, errors.New("size value too small")
+	}
+	return &Pool{
+		factory:   fn,
+		resources: make(chan io.Closer, size),
+	}, nil
+}
+
+/**
+从池中获取一个资源
+*/
+func (p *Pool) Acquire() (io.Closer, error) {
+	select {
+	case r, ok := <-p.resources:
+		if !ok {
+			return nil, ErrPoolClosed
+		}
+		return r, nil
+	default:
+		log.Println("Acquire:", "New Resource")
+		return p.factory()
+	}
+}
+
+/**
+将资源释放回池中
+*/
+func (p *Pool) Release(r io.Closer) {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	if p.closed {
+		r.Close()
+		return
+	}
+
+	select {
+	case p.resources <- r:
+		log.Println("Release:", "In Queue")
+	default:
+		log.Println("Release:", "Closing")
+		r.Close()
+	}
+}
+
+/**
+关闭资源池
+*/
+func (p *Pool) Close() {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	if p.closed {
+		return
+	}
+
+	p.closed = true
+
+	close(p.resources)
+
+	for r := range p.resources {
+		r.Close()
+	}
+}
